38af9fae52d4f30b59879aa653b4c220be2859a9
Tpikl <pickles.tayler@gmail.com>
2022-10-28 08:12:10 -0700

    wksp

--- Scripts/Tpikl/shortcuts.ps1
+++ Scripts/Tpikl/shortcuts.ps1
@@ -2,11 +2,13 @@ New-Module shortcuts {
 
 # --- Functions ----------------
   function expl { explorer . }  # Open explorer folder in current directory.
+  function wksp { cd C:\wksp }  # CD to workspace dir.
 # --- Functions ----------------
 
 
 # --- Exports ------------------
   Export-ModuleMember expl
+  Export-ModuleMember wksp
 # --- Exports ------------------
 
 } | Import-Module
